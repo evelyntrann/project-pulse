de0cd7c3c690033884012edc3124542df5c55360
msthill <micah.sthill123@gmail.com>
2026-04-20 16:37:18 -0500

    Revert "setup student account"
    This reverts commit 9d39ca3b943640dce90e282df9e764f1bf1211b4.

--- backend/src/main/java/com/projectpulse/registration/dto/RegisterResponse.java
+++
@@ -1,9 +0,0 @@
-package com.projectpulse.registration.dto;
-
-public record RegisterResponse(
-        Long id,
-        String email,
-        String firstName,
-        String lastName,
-        String role
-) {}
